1eba0dee15a0867660b24e32d246ec26d4113097
kiurek <kubixk21@gmail.com>
2023-06-12 21:46:36 +0200

    home page images, content update

--- flutter_2kbr/lib/pages/home_page.dart
+++ flutter_2kbr/lib/pages/home_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_2kbr/pages/login_page.dart';
 import 'package:flutter_2kbr/pages/offer_page.dart';
+import 'package:flutter_2kbr/pages/sills/sill_pattern_page.dart';
 import 'package:flutter_2kbr/providers/auth_provider.dart';
 import 'package:flutter_2kbr/widgets/custom_appbar.dart';
 import 'package:flutter_2kbr/widgets/navigate_animation.dart';
 import 'package:provider/provider.dart';
+
+import 'fronts/front_pattern_page.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -21,96 +24,229 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final authProvider = Provider.of<AuthProvider>(context);
 
+    var size = MediaQuery.of(context).size;
+    final double horizontalPadding = size.width > 800 ? size.width * 0.2 : 0.9;
+
     return WillPopScope(
-      onWillPop: () async => false,
-      child: Scaffold(
-        appBar: CustomAppBar(
-          onActionPressed: () {
-            final authProvider =
-                Provider.of<AuthProvider>(context, listen: false);
-            if (authProvider.isLoggedIn) {
-              authProvider.logout();
-            }
-          },
-        ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/klasyczna.jpg'),
-                      fit: BoxFit.cover,
-                    ),
+        onWillPop: () async => false,
+        child: Scaffold(
+            appBar: CustomAppBar(
+              onActionPressed: () {
+                final authProvider =
+                    Provider.of<AuthProvider>(context, listen: false);
+                if (authProvider.isLoggedIn) {
+                  authProvider.logout();
+                }
+              },
+            ),
+            body: SingleChildScrollView(
+                child: Center(
+                    child: Column(children: [
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/klasyczna.jpg'),
+                    fit: BoxFit.cover,
                   ),
-                  height: 350,
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Padding(
-                      padding: EdgeInsets.only(
-                          left: MediaQuery.of(context).size.width / 15),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Dom tworzą ludzie',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 50,
-                              fontWeight: FontWeight.w900,
-                            ),
+                ),
+                height: 350,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                        left: MediaQuery.of(context).size.width / 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Dom tworzą ludzie',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 50,
+                            fontWeight: FontWeight.w900,
                           ),
-                          Text(
-                            'oraz meble...',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 50,
-                              fontWeight: FontWeight.w900,
-                            ),
+                        ),
+                        Text(
+                          'oraz meble...',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 50,
+                            fontWeight: FontWeight.w900,
                           ),
-                          ElevatedButton(
-                            onPressed: () {
-                              navigateWithoutAnimation(context, OfferPage());
-                            },
-                            style: ButtonStyle(
-                              backgroundColor:
-                                  MaterialStateProperty.all(Colors.orange),
-                            ),
-                            child: const Text('Zobacz ofertę'),
-                          )
-                        ],
-                      ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            navigateWithoutAnimation(context, OfferPage());
+                          },
+                          style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.orange),
+                          ),
+                          child: const Text('Zobacz ofertę'),
+                        )
+                      ],
                     ),
                   ),
                 ),
-                Column(
-                  children: [
-                    Text(
-                      'dsad',
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: MediaQuery.of(context).size.width / 15,
+                        top: MediaQuery.of(context).size.height / 15),
+                    child: Text(
+                      'Meble to nasza pasja',
                       style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 50,
+                        color: Color.fromARGB(164, 164, 164, 164),
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: MediaQuery.of(context).size.width / 15),
+                    child: Text(
+                      'Jesteśmy producentem wysokiej jakości frontów meblowych',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 70,
                         fontWeight: FontWeight.w900,
                       ),
                     ),
-                    Text(
-                      'dasd',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 50,
-                        fontWeight: FontWeight.w900,
-                      ),
+                  ),
+                  SizedBox(
+                      height:
+                          5), // Dodaje odstęp pomiędzy tekstem a kontenerami
+                  Padding(
+                    padding: EdgeInsets.only(
+                      left: MediaQuery.of(context).size.width / 15,
                     ),
-                  ],
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+                    child: Row(
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage('assets/images/home1.jpg'),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              height: MediaQuery.of(context).size.height * 0.7,
+                              width: MediaQuery.of(context).size.width * 0.3,
+                              child: Center(
+                                child: Padding(
+                                  padding: EdgeInsets.only(
+                                      left: MediaQuery.of(context).size.width /
+                                          15),
+                                  child: Text(
+                                    'Fronty meblowe na zamówienie',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 40,
+                                      fontWeight: FontWeight.w900,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                      'assets/images/parapety_header2.jpg'),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              height: MediaQuery.of(context).size.height * 0.2,
+                              width: MediaQuery.of(context).size.width * 0.3,
+                              child: Center(
+                                child: Padding(
+                                  padding: EdgeInsets.only(
+                                      left: MediaQuery.of(context).size.width /
+                                          15),
+                                  child: Text(
+                                    'Parapety',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 40,
+                                      fontWeight: FontWeight.w900,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                            height: 5), // Dodaje odstęp pomiędzy kontenerami
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage('assets/images/home2.jpg'),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              height: MediaQuery.of(context).size.height * 0.3,
+                              width: MediaQuery.of(context).size.width * 0.4,
+                              child: Center(
+                                child: Padding(
+                                    padding: EdgeInsets.only(
+                                        left:
+                                            MediaQuery.of(context).size.width /
+                                                15),
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        navigateWithoutAnimation(
+                                            context, FrontPatternPage());
+                                      },
+                                      child:
+                                          const Text('Bogaty katalog frontów'),
+                                    )),
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage('assets/images/home3.jpg'),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              height: MediaQuery.of(context).size.height * 0.7,
+                              width: MediaQuery.of(context).size.width * 0.4,
+                              child: Center(
+                                child: Padding(
+                                  padding: EdgeInsets.only(
+                                      left: MediaQuery.of(context).size.width /
+                                          15),
+                                  child: Text(
+                                    'Współpraca z producentami mebli',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.w900,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ])))));
   }
 
   void _navigateToLogin() {
