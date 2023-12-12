4d44e75bd942e3ad0a9b74a1cffe31691632b0b7
Kuba Kurek <79983272+kiurek@users.noreply.github.com>
2023-12-13 00:20:26 +0100

    Updated models query

--- SQLqueries/Models_query.sql
+++ SQLqueries/Models_query.sql
@@ -1,32 +1,352 @@
-INSERT INTO Models
-VALUES ('Ramiak', 'Fronty', 100.00)
+--INSERT INTO Models (ModelName, Type, Value)
+--VALUES ('ABO', 'Fronty frezowane', 0 )
 
-INSERT INTO Models
-VALUES ('Sprint', 'Fronty', 93.00)
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Andros', 'Fronty frezowane', 0 )
 
-INSERT INTO Models
-VALUES ('PBO', 'Fronty', 10.00)
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Anse', 'Fronty frezowane', 0 )
 
-INSERT INTO Models
-VALUES ('PGO', 'Fronty', 100.00)
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('BAR', 'Fronty frezowane', 0 )
 
-INSERT INTO Models
-VALUES ('G', 'Parapety', 0.00)
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('BBO', 'Fronty frezowane', 0 )
 
-INSERT INTO Models
-VALUES ('B', 'Parapety', 0.00)
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Boston', 'Fronty frezowane', 0 )
 
-INSERT INTO Models
-VALUES ('C', 'Parapety', 0.00)
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Breve', 'Fronty frezowane', 0 )
 
-INSERT INTO Models
-VALUES ('E', 'Parapety', 0.00)
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Casa', 'Fronty frezowane', 0 )
 
-INSERT INTO Models
-VALUES ('F', 'Parapety', 0.00)
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('CBO', 'Fronty frezowane', 0 )
 
-INSERT INTO Models
-VALUES ('G', 'Parapety', 0.00)
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Delos', 'Fronty frezowane', 0 )
 
-INSERT INTO Models
-VALUES ('I', 'Parapety', 0.00)
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Devon', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('EBO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('FBO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Folki', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('GGO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Griff', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('HGO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('IGO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('JGO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Kanna', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Kantry', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Karbo', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('KBZ', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Klasyk', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Kwadro', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Lame', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('LBO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Lindos', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('London', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Lurosa', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Madera', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Malaga', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Marco', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('MGO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Milos', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Modena', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Moira', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Mona', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Monty', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Neapol', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('NGO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('No name 2', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('No name 3', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('OIO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Paloma', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Parma', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Paros', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Pati', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Pianta', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Pillos', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Pillow', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Portyk', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Prada', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('QGO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Ramiak', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Ramko', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Ramona', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Rampel 60', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Rampel', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Rapino', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Rawa', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Ravena', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Rena', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Roma', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('RBO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Sandra', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Severo', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Roxana', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Sprint', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Stripe', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('SIO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Telaio', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Texas', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Tamara', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Torino', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('UGO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('TGO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Vega', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Verona', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Waza', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('WDO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('VIO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('XDO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('Wenecja', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('ZGO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('YDO', 'Fronty frezowane', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PAO', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PBO', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PCO', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PDO', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PEO', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PFO', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PGO', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PHO', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PIO', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PJO', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PKO', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PLO', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PAZ', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PBZ', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PCZ', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PDZ', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PEZ', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PFZ', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PGZ', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PHZ', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PIZ', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PJZ', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PKZ', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('PLZ', 'Fronty gładkie', 0 )
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('B', 'Parapet', 0)
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('C', 'Parapet', 0)
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('E', 'Parapet', 0)
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('G', 'Parapet', 0)
+
+INSERT INTO Models (ModelName, Type, Value)
+VALUES ('I', 'Parapet', 0)
+
+UPDATE Models
+SET Value = 283
+WHERE Type = 'Fronty frezowane'
+
+UPDATE Models
+SET Value = 223
+WHERE Type = 'Fronty gładkie'
